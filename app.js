7ef69cef54ab32b0b66a592293131134f9ddde11
Mikel <mikelbrierly@gmail.com>
2015-12-06 20:25:40 -0700

    initial commit

---
+++ app.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+var user_file = document.getElementById('user_file');
+var button = document.getElementById('submit');
+var output = document.getElementById('output');
+// var vr_ify = function(){
+//   output.innerHTML = user_file;
+// }
+
+
+
+
+
+button.addEventListener("click", vr_ify);
+});
